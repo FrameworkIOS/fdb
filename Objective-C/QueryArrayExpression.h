e36c020230bfd394116f752d46bbca6f24a1b159
krishpranav <krisna.pranav@gmail.com>
2023-07-09 15:58:37 +0530

    QueryArrayExpression

--- Objective-C/QueryArrayExpression.h
+++ Objective-C/QueryArrayExpression.h
@@ -3,11 +3,24 @@
 @class QueryExpression;
 @class QueryVariableExpression;
 
+
 NS_ASSUME_NONNULL_BEGIN
 
-@interface QueryArrayExpression: NSObject
+@interface QueryArrayExpression : NSObject
 
-+ (QueryVariableExpression) variableWithName: (NSString*) name;
++ (QueryVariableExpression*) variableWithName: (NSString*)name;
+
++ (QueryExpression*) any: (QueryVariableExpression*)variable
+                         in: (QueryExpression*)inExpression
+                  satisfies: (QueryExpression*)satisfies;
+
++ (QueryExpression*) anyAndEvery: (QueryVariableExpression*)variable
+                                 in: (QueryExpression*)inExpression
+                          satisfies: (QueryExpression*)satisfies;
+
++ (QueryExpression*) every: (QueryVariableExpression*)variable
+                           in: (QueryExpression*)inExpression
+                    satisfies: (QueryExpression*)satisfies;
 
 - (instancetype) init NS_UNAVAILABLE;
 
