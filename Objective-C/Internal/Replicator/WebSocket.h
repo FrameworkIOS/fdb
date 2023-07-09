20ff718a1cadb02fcb8915a6736c5ad430224f29
krishpranav <krisna.pranav@gmail.com>
2023-07-09 14:41:30 +0530

    Internal: Replicator[WebSocket]

--- Objective-C/Internal/Replicator/WebSocket.h
+++ Objective-C/Internal/Replicator/WebSocket.h
@@ -2,12 +2,18 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WebSocket: NSObject<NSURLSessionStreamDelegate>
+
+@interface WebSocket : NSObject <NSURLSessionStreamDelegate>
 
 + (C4SocketFactory) socketFactory;
+
 + (nullable NSString*) webSocketAcceptHeaderForKey: (NSString*)key;
-+ (NSArray*) parseCookies: (NSString*)cookie;
+
++ (NSArray*) parseCookies: (NSString*) cookie;
+
++ (nullable NSString*) getNetworkInterfaceName: (NSString*)name error: (NSError**)outError;
 
 @end
+
 
 NS_ASSUME_NONNULL_END
